8fd8cda42b478edd840e2d219d01d04543d70c4f
Stephen Anderson <anderssj@usc.edu>
2019-01-16 08:50:29 -0800

    get

--- array.js
+++ array.js
@@ -3,12 +3,35 @@ const memory = require('./memory')
 class Array {
   constructor() {
     this.length = 10
+    this.capacity = 10
     this.ptr = memory.allocate(this.length)
+    //[][][][][]
   }
-  _resize = () => {}
+  _resize = size => {
+    const oldPtr = this.ptr
+    this.ptr = memory.allocate(size)
+    if (this.ptr === null) {
+      throw new Error('Out of memory')
+    }
+    memory.copy(this.ptr, oldPtr, this.length)
+    memory.free(oldPtr)
+    this.capacity = size
+  }
+
   push = item => {
-    this._resize()
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO)
+    }
     memory.set(this.ptr + this.length, item)
+    this.length++
+  }
+
+  get = index => {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error')
+    }
+    return memory.get(this.ptr + index)
   }
   pop = () => {}
 }
+Array.SIZE_RATIO = 3
