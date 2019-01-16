6c0b476f9015854162f876e8b87f585b723c1245
Stephen Anderson <anderssj@usc.edu>
2019-01-16 08:29:30 -0800

    psuedocode

--- array.js
+++ array.js
@@ -1,8 +1,14 @@
 const memory = require('./memory')
 
 class Array {
-  constructor() {}
-
-  push = () => {}
+  constructor() {
+    this.length = 10
+    this.ptr = memory.allocate(this.length)
+  }
+  _resize = () => {}
+  push = item => {
+    this._resize()
+    memory.set(this.ptr + this.length, item)
+  }
   pop = () => {}
 }
